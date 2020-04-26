89e5c74b820a4d096a10a65a9e15431dc807e0c7
Gustavo de Leon <gdeleontobias@gmail.com>
2020-04-25 18:27:34 -0600

    redes neuronales

--- HDT7.R
+++ HDT7.R
@@ -1,5 +1,5 @@
 library(caret)
-library(dummies)
+library(dummy)
 library(plyr)
 library(dplyr)
 library(e1071)
@@ -10,7 +10,7 @@ library(RWeka)
 library(neural)
 library(neuralnet)
 
-#Modelo de Regresi?n log?stica
+#Redes Neuronales 
 
 setwd("C:/Users/Gustavo/Desktop/SEPTIMO SEMESTRE/MINERIA/HDT7/HDT7-MINERIA")
 #setwd("C:/Users/alber/Documents/UVG/Septimo semestre/Mineria de Datos/Hoja-Trabajo-6/Hoja-de-trabajo-06")
@@ -27,10 +27,7 @@ trainImportantes$grupo<-km$cluster
 
 trainImportantes$grupo <- mapvalues(trainImportantes$grupo, c(1,2,3), c("Intermedio","Barato","Caro"))
 
-#trainImportantes<-cbind(trainImportantes,dummy(trainImportantes$grupo,verbose = T))
-#colnames(trainImportantes)[13] <- "EsBarata"
-#colnames(trainImportantes)[14] <- "EsCara"
-#colnames(trainImportantes)[15] <- "EsIntermedia"
+#trainImportantes<-cbind(trainImportantes,dummy(trainImportantes,verbose = T))
 
 porcentaje<-0.7
 corte <- sample(nrow(trainImportantes),nrow(trainImportantes)*porcentaje)
@@ -40,21 +37,51 @@ test<-trainImportantes[-corte,]
 #-------------------------------------------------
 # Red Neuronal con nnet
 #-------------------------------------------------
-
-trainImportantes$grupo <- as.factor(trainImportantes$grupo)
-modelo.nn2 <- nnet(grupo~.,data = trainImportantes,subset = corte, size=2, rang=0.1,
-                   decay=5e-4, maxit=200) 
-
+modelo.nn2 <- nnet(as.factor(grupo)~.,data=train, size=5, rang=0.0000001,decay=5e-4, maxit=500) 
 prediccion2 <- as.data.frame(predict(modelo.nn2, newdata = test[,1:11]))
-
 columnaMasAlta<-apply(prediccion2, 1, function(x) colnames(prediccion2)[which.max(x)])
-
-test$prediccion2<-columnaMasAlta #Se le añade al grupo de prueba el valor de la predicción
-test$grupo <- as.factor(test$grupo)
+test$prediccion2<-columnaMasAlta
 test$prediccion2 <- as.factor(test$prediccion2)
-cfm<-confusionMatrix(test$prediccion2,test$grupo)
+cfm<-confusionMatrix(test$prediccion2,as.factor(test$grupo))
 cfm
+#-------------------------------------------------
 
 #-------------------------------------------------
+# Red Neuronal con RWeka
+#-------------------------------------------------
+NB <- make_Weka_classifier("weka/classifiers/functions/MultilayerPerceptron")
+NB 
+WOW(NB)
+nnodos='4'
+
+modelo.bp<-NB(as.factor(grupo)~., data=trainImportantes,subset = corte,
+              control=Weka_control(H=nnodos, N=1000, G=TRUE), options=NULL)
+test$prediccionWeka<-predict(modelo.bp, newdata = test[,1:11])
+cfmWeka<-confusionMatrix(test$prediccionWeka,as.factor(test$grupo))
+cfmWeka
+
+#-------------------------------------------------
+# Red Neuronal con caret
+#-------------------------------------------------
+
+modeloCaret <- train(as.factor(grupo)~.,data=train, method="nnet", trace=F)
+test$prediccionCaret<-predict(modeloCaret, newdata = test[,1:11])
+cfmCaret<-confusionMatrix(test$prediccionCaret,as.factor(test$grupo))
+cfmCaret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
